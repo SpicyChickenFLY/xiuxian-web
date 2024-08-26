c1c92fb15b56a6ef16cefe2ab067410d3b5fb1c3
SpicyChickenFLY <xixiayuchao@gmail.com>
2024-08-26 16:48:53 +0800

    back: 完善后端加载文件功能

--- back/app.py
+++ back/app.py
@@ -1,31 +1,129 @@
-"""自动化机器人管理服务"""
+"""自动化任务管理器及配置服务"""
+
+import threading
+import os
+import json
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_restful import Api
 
-from task import TaskMgr
+from task import Task
+
+class TaskMgr():
+    """自动化任务管理器"""
+
+    def __init__(self) -> None:
+        self._running = False
+        self._thread = None
+        self._task_map = {}
+
+        # 读取本地任务信息
+        for root, _, files in os.walk('profiles'):
+            for file in files:
+                file_path = os.path.join(root, file)
+                try:
+                    with open(file_path, "r", encoding="utf-8") as rf:
+                        print(file_path)
+                        self.create_task(json.load(rf))
+                except Exception as e:
+                    print(f"根据配置文件{file_path}创建任务失败 {type(e)} {e}")
+
+    def _run(self):
+        """自动化程序启动"""
+        while True:
+            for _, task in self._task_map.items():
+                if not self._running:
+                    return
+                task.run()
+
+    def is_running(self):
+        """获取自动化任务启停状态"""
+        return self._running
+
+    def start(self):
+        """启动自动化任务"""
+        if self._running:
+            print('started')
+            return
+        self._running = True
+        self._thread = threading.Thread(target=self._run)
+        self._thread.start()
+
+    def stop(self):
+        """停止自动化任务"""
+        if not self._running:
+            print('stopped')
+            return
+        self._running = False
+        if self._thread:
+            self._thread.join()
+
+    def list_tasks(self):
+        """返回自动化任务列表"""
+        task_list = []
+        for name, task in self._task_map.items():
+            task_list.append({ "name": name, "modules": task.get_brief() })
+        return task_list
+
+    def get_task(self, task_id):
+        """返回自动化任务详情"""
+        self._task_map[task_id].export()
+
+    def create_task(self, task_profile):
+        """创建新的自动化任务"""
+        if self._running:
+            self.stop()
+            name = task_profile['name']
+            self._task_map[name] = Task(name, task_profile)
+            self.start()
+        else:
+            name = task_profile['name']
+            self._task_map[name] = Task(name, task_profile)
+        return name
+
+    def update_task(self, task_id, task_profile):
+        """创建新的自动化任务"""
+        if self._running:
+            self.stop()
+            self._task_map[task_id].update(task_profile)
+            self.start()
+        else:
+            self._task_map[task_id].update(task_profile)
+        return task_id
+
+    def delete_task(self, task_id):
+        """创建新的自动化任务"""
+        if self._running:
+            self.stop()
+            self._task_map.pop(task_id)
+            self.start()
+        else:
+            self._task_map.pop(task_id)
+        return task_id
+taskMgr = TaskMgr()
+
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-taskMgr = TaskMgr()
-
 @app.route('/task/list', methods=["GET"])
 def list_tasks():
     """列出所有管理任务"""
-    return jsonify({"taskList": taskMgr.list_tasks()}), 200
+    return jsonify(taskMgr.list_tasks()), 200
 
-@app.route('/task/<string: name>', methods=["GET"])
-def get_task():
-    """列出所有管理任务"""
-    return jsonify({"taskList": taskMgr.list_tasks()}), 200
+@app.route('/task/<string:name>', methods=["GET"])
+def get_task(name):
+    """查询指定管理任务"""
+    return jsonify({"taskList": taskMgr.get_task(name)}), 200
 
 @app.route('/task/create', methods=["POST"])
 def create_task():
     """创建管理任务"""
     name = taskMgr.create_task(request.form)
+    if name == "":
+        return jsonify({"msg": "创建失败"}), 400
     return jsonify({"taskId": name}), 200
 
 @app.route('/task/update', methods=["POST"])
--- back/task.py
+++ back/task.py
@@ -1,8 +1,6 @@
 """自动化机器人"""
 
-import re
 import time
-import threading
 
 from utils import Bot
 
@@ -18,32 +16,29 @@ from xuan_shang_ling import XuanShangLing
 from zong_meng_ren_wu import ZongMenRenWu
 
 
-
 # class OperateBot(threading.Thread):
 class Task(Bot):
     """自动化任务"""
 
     def __init__(self, name, profile) -> None:
-        bot_config = profile['bot'] if 'bot' in profile else {}
+        bot_config = profile["bot"] if "bot" in profile else {}
         Bot.__init__(self, name, bot_config)
 
-        self._running = False
-        self._thread = None
         self._module_wait_map = {}
         self._code_module_map = {
-            "lt": LingTian,
-            "bc": BuChang,
-            "qd": QianDao,
-            "dy": DanYao,
-            "sx": ShuangXiu,
-            "zm": ZongMenRenWu,
-            "xsl": XuanShangLing,
-            "mj": MiJing,
-            "xl": XiuLian,
-            "fs": FangShi,
+            "灵田": LingTian,
+            "补偿": BuChang,
+            "签到": QianDao,
+            "宗门丹药": DanYao,
+            "双修": ShuangXiu,
+            "宗门任务": ZongMenRenWu,
+            "悬赏令": XuanShangLing,
+            "秘境": MiJing,
+            "修炼": XiuLian,
+            "坊市": FangShi,
         }
         self.modules = {}
-        module_profile = profile['module'] if 'module_config' in profile else {}
+        module_profile = profile["module"] if "module_config" in profile else {}
         for code, module in self._code_module_map.items():
             module_config = module_profile[code] if code in module_profile else {}
             self.modules[code] = module(module_config)
@@ -56,7 +51,8 @@ class Task(Bot):
         #     "progress": self.progress,
         # }
 
-    def module_list(self):
+    def get_brief(self):
+        """获取任务模块简要信息"""
         module_map = {}
         for code, module in self.modules.items():
             module_map[code] = module.enable
@@ -82,68 +78,3 @@ class Task(Bot):
                 return
         # 没有可执行模块
         time.sleep(1)
-
-class TaskMgr():
-    """修仙机器人任务管理器"""
-
-    def __init__(self) -> None:
-        self._running = False
-        self._thread = None
-        self._task_map = {}
-
-        # 浏览本地profile
-
-    def _run(self):
-        """自动化程序启动"""
-        while True:
-            for task in self._task_map:
-                if not self._running:
-                    return
-                task.run()
-
-    def is_running(self):
-        """获取机器人启停状态"""
-        return self._running
-
-    def start(self):
-        """启动自动化任务"""
-        self._running = True
-        self._thread = threading.Thread(target=self._run)
-
-    def stop(self):
-        """停止自动化任务"""
-        self._running = False
-        if self._thread:
-            self._thread.join()
-
-    def list_tasks(self):
-        """返回自动化任务列表"""
-        task_list = []
-        for name, task in self._task_map.items():
-            task_list.append({ "name": name, "modules": task.module_list() })
-        return task_list
-
-    def get_task(self, task_id):
-        """返回自动化任务详情"""
-        self._task_map[task_id].export()
-
-    def create_task(self, task_profile):
-        """创建新的自动化任务"""
-        self.stop()
-        self._task_map[""] = Task("", task_profile)
-        self.start()
-        return ""
-
-    def update_task(self, task_id, task_profile):
-        """创建新的自动化任务"""
-        self.stop()
-        self._task_map[task_id].update(task_profile)
-        self.start()
-        return ""
-
-    def delete_task(self, task_id):
-        """创建新的自动化任务"""
-        self.stop()
-        self._task_map.pop(task_id)
-        self.start()
-        return ""
