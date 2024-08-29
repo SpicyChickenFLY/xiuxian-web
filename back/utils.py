7847baac433adb15cd23d46dd4d85840eed4c496
SpicyChickenFLY <xixiayuchao@gmail.com>
2024-08-30 07:30:58 +0800

    新增悬赏令选取算法

--- back/utils.py
+++ back/utils.py
@@ -2,9 +2,25 @@ import re
 
 class FuncUtil():
     def find_one_int(self, resp, regex):
-        return int(re.findall(r"(\d+)分钟", resp)[0])
+        return int(re.findall(regex, resp)[0])
     def find_one_float(self, resp, regex):
-        return float(re.findall(r"(\d+\.?\d*)分钟", resp)[0])
-    def find_all(self, resp, regex):
-        pass
+        return float(re.findall(regex, resp)[0])
+    def chooseTask(self, resp):
+        reward_map = {} # TODO: 加载奖励价值表
+        probailities = re.findall(r"完成几率(\d+)", resp)
+        rewards = re.findall(r"可能额外获得：(.*):", resp)
+        if len(probailities) == 0:
+            return 1
+        rewards_values = [reward_map[i] if i in reward_map else i for i in rewards]
+        tasks = sorted(
+            list(zip(probailities, rewards_values)),
+            key=lambda x: x[1],
+            reverse=True,
+        )
+        choice = 1
+        while choice <= len(tasks) and int(tasks[choice - 1][0]) < 80:
+            choice += 1
+        if choice > len(tasks):
+            choice = 1
+        return choice
 
