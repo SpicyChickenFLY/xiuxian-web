003aac133468c822e9f27145c99fafb9f36e8890
SpicyChickenFLY <xixiayuchao@gmail.com>
2024-09-25 18:50:48 +0800

    由于各个开发环境差异,脚本应该各自开发,且互不影响

--- start_back.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-WORKSPACE=$(dirname $(readlink -f "$0"))
-
-cd $WORKSPACE/back
-/mnt/Mine/Code/python/venv/bin/python app.py
--- start_front.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-WORKSPACE=$(dirname $(readlink -f "$0"))
-
-cd $WORKSPACE/front
-npm run dev
